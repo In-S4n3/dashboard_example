557442ec73ddbd5b48faabebb3cb02f96ff7dadd
In-S4n3 <tiagodcpp@gmail.com>
2024-01-16 15:19:54 +0000

    finished step 11

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -18,41 +18,45 @@ export default async function InvoicesTable({
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
-            {invoices?.map((invoice) => (
-              <div
-                key={invoice.id}
-                className="mb-2 w-full rounded-md bg-white p-4"
-              >
-                <div className="flex items-center justify-between border-b pb-4">
-                  <div>
-                    <div className="mb-2 flex items-center">
-                      <Image
-                        src={invoice.image_url}
-                        className="mr-2 rounded-full"
-                        width={28}
-                        height={28}
-                        alt={`${invoice.name}'s profile picture`}
-                      />
-                      <p>{invoice.name}</p>
+            {invoices?.map((invoice) => {
+              return (
+                <div
+                  key={invoice.id}
+                  className="mb-2 w-full rounded-md bg-white p-4"
+                >
+                  <div className="flex items-center justify-between border-b pb-4">
+                    <div>
+                      <div className="mb-2 flex items-center">
+                        <Image
+                          src={invoice.customer.image_url}
+                          className="mr-2 rounded-full"
+                          width={28}
+                          height={28}
+                          alt={`${invoice.customer.name}'s profile picture`}
+                        />
+                        <p>{invoice.customer.name}</p>
+                      </div>
+                      <p className="text-sm text-gray-500">
+                        {invoice.customer.email}
+                      </p>
                     </div>
-                    <p className="text-sm text-gray-500">{invoice.email}</p>
+                    <InvoiceStatus status={invoice.status} />
                   </div>
-                  <InvoiceStatus status={invoice.status} />
+                  <div className="flex w-full items-center justify-between pt-4">
+                    <div>
+                      <p className="text-xl font-medium">
+                        {formatCurrency(invoice.amount)}
+                      </p>
+                      <p>{formatDateToLocal(invoice.date.toISOString())}</p>
+                    </div>
+                    <div className="flex justify-end gap-2">
+                      <UpdateInvoice id={invoice.id} />
+                      <DeleteInvoice id={invoice.id} />
+                    </div>
+                  </div>
                 </div>
-                <div className="flex w-full items-center justify-between pt-4">
-                  <div>
-                    <p className="text-xl font-medium">
-                      {formatCurrency(invoice.amount)}
-                    </p>
-                    <p>{formatDateToLocal(invoice.date)}</p>
-                  </div>
-                  <div className="flex justify-end gap-2">
-                    <UpdateInvoice id={invoice.id} />
-                    <DeleteInvoice id={invoice.id} />
-                  </div>
-                </div>
-              </div>
-            ))}
+              );
+            })}
           </div>
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
@@ -86,23 +90,23 @@ export default async function InvoicesTable({
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
                       <Image
-                        src={invoice.image_url}
+                        src={invoice.customer.image_url}
                         className="rounded-full"
                         width={28}
                         height={28}
-                        alt={`${invoice.name}'s profile picture`}
+                        alt={`${invoice.customer.name}'s profile picture`}
                       />
-                      <p>{invoice.name}</p>
+                      <p>{invoice.customer.name}</p>
                     </div>
                   </td>
                   <td className="whitespace-nowrap px-3 py-3">
-                    {invoice.email}
+                    {invoice.customer.email}
                   </td>
                   <td className="whitespace-nowrap px-3 py-3">
                     {formatCurrency(invoice.amount)}
                   </td>
                   <td className="whitespace-nowrap px-3 py-3">
-                    {formatDateToLocal(invoice.date)}
+                    {formatDateToLocal(invoice.date.toISOString())}
                   </td>
                   <td className="whitespace-nowrap px-3 py-3">
                     <InvoiceStatus status={invoice.status} />
